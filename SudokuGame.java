520ff4d8afaab00d38e7e2636190b0b5476d27d2
Matt <mjcantin@yahoo.com>
2025-10-13 15:46:58 -0600

    Revert "Changed Sudoku"

--- SudokuGame.java
+++ SudokuGame.java
@@ -1,9 +1,6 @@
 import java.util.Optional;
 
 /**
- * 
- * 
- * 
  * A number placement puzzle on a 9x9 grid.
  * The objective is to fill the grid with digits from 1 to 9 so that each
  * column, row, and 3x3 subgrid contains all digits without repetition.
@@ -22,12 +19,6 @@ class SudokuGame implements Game {
     @Override
     public Optional<Integer> play() {
         System.out.println("[Playing Sudoku - Placeholder]");
-        
-        System.out.println("Welcome to Sudoku!");
-        System.out.println("fill the grid with digits 1 to 9");
-        System.out.print("so that each column and row and 3x3 subgrid");
-        System.out.print("containts all digits without repetition");
-        
         return Optional.empty();
     }
 }
